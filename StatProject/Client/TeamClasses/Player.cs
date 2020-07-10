4f6be3d139d8a9b59b84687c3e63a9c68059b50e
Mark Felchlin <mdfelchlin@gmail.com>
2020-07-09 20:06:35 -0700

    Add project files.

---
+++ StatProject/Client/TeamClasses/Player.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace StatProject.Client.TeamClasses
+{
+    public class Player
+    {
+        public int JerseyNumber { get; set; }
+        public string Name { get; set; }
+        public string Position { get; set; }
+    }
+}
